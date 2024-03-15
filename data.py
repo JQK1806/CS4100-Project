92bca28c8e8fc3e1716c86ac43103c37de59583e
JQK1806 <king.jos@northeastern.edu>
2024-03-15 15:04:51 -0400

    Created dataset
    Dataset that contains random values for energy usage in each hour of the day

---
+++ data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+# Values based on monthly kWh from 
+    # https://cornhusker-power.com/rebates/household-appliances/
+    # https://www.energysage.com/electricity/house-watts/how-many-watts-does-an-electric-furnace-use/
+# Estimation of daily kWh used (min, max)
+appliances = {
+    'Washer': (0, 0.1),            # Adjusted for hourly usage
+    'Dryer': (0, 0.3),             # Adjusted for hourly usage
+    'Lighting': (0, 0.05),         # Adjusted for hourly usage
+    'Heating': (0, 3)              # Adjusted for hourly usage
+}
+
+n_days = 5
+
+data = []
+for day in range(n_days):
+    day_data = []
+    for hour in range(24):
+        entry = {'Day': day+1, 'Hour': hour}
+        for appliance, (low, high) in appliances.items():
+            usage = np.random.uniform(low, high)
+            entry[appliance] = usage
+        day_data.append(entry)
+    day_df = pd.DataFrame(day_data)
+    
+    total_day_usage = day_df.drop(columns=['Day', 'Hour']).sum().to_dict()
+    total_day_usage['Day'] = day + 1
+    total_day_usage['Hour'] = 'Total'
+    data.extend(day_data)
+    data.append(total_day_usage)
+
+df = pd.DataFrame(data)
+
+
+print(df.head(30))
+
