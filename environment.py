9f49312c512f5b3df5fb9b131773076b67721f1a
michelleli04 <michelle6465li@gmail.com>
2024-03-29 13:23:25 -0400

    New environment

--- environment.py
+++ environment.py
@@ -1,22 +1,22 @@
 import gym
 from gym import spaces
 import numpy as np
+import itertools
 
 class SmartHomeEnv(gym.env):
-    def __init__(self, params):
+    def __init__(self):
         super(SmartHomeEnv, self).__init__()
-        self.params = params
-        self.num_appliances = params['num_appliances']
-        self.appliances = params['appliances']
-        self.appliance_energy_consumption = [appliance['energy_consumption'] for appliance in self.appliances]
-
-        self.action_space = spaces.MultiBinary(self.num_appliances)  # On/Off for each appliance
-        self.observation_space = spaces.Box(low=0, high=1, shape=(self.num_appliances,), dtype=np.float32)  # Status of appliances
+        self.num_zones = 9
+        self.observation_space = spaces.Discrete(2 ** self.num_zones)  # Number of possible states, 2^9 - 0=empty, 1=occupied
+        self.action_space = spaces.MultiDiscrete([4] * self.num_zones)  # Each zone has 4 possible actions:0,1,2,3
+        self.states = list(itertools.product([0, 1], repeat=self.num_zones))  # All possible states, (zone #, occupancy)
         self.state = self.reset()
     def reset(self):
-        pass
+        self.state = np.zeros(self.num_zones, dtype=int)  # Reset all zones to unoccupied - 0
+        return self.encode_state()
+
     def step(self, action):
+        # ppl move around, current temp of each zone should be updated
         pass
-    
 
 
